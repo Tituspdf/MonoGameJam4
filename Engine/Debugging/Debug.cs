ff04d600b5ee61113bf6c195d9c0ea44a745cedc
Tituspdf <titus.huebner@gmail.com>
2022-01-03 21:13:50 +0100

    created logging class

---
+++ Engine/Debugging/Debug.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MonoGameJam4.Engine.Debugging
+{
+    public static class Debug
+    {
+        private static readonly Action<object> Logging = Console.WriteLine;
+        
+        public static void Log(object o)
+        {
+            Logging.Invoke(o);
+        }
+
+        public static void LogWarning(object o)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Logging.Invoke(o);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void LogError(object o)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Logging.Invoke(o);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}

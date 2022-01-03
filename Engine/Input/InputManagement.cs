09a96d21ba982260d66e6eba049f07704c6dd14a
Tituspdf <titus.huebner@gmail.com>
2022-01-03 20:28:05 +0100

    added Key class

--- Engine/Input/InputManagement.cs
+++ Engine/Input/InputManagement.cs
@@ -11,4 +11,26 @@ namespace MonoGameJam4.Engine
         {
         }
     }
+
+    public class Key
+    {
+        private readonly Keys _key;
+        private bool _isPressed;
+
+        public event Action Invoked;
+
+        public Key(Keys key)
+        {
+            _key = key;
+        }
+
+        private void Update(KeyboardState keyboardState)
+        {
+            bool state = keyboardState.IsKeyDown(_key);
+
+            if (!_isPressed) Invoked?.Invoke();
+
+            _isPressed = state;
+        }
+    }
 }

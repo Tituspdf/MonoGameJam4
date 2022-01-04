a36bc2da2a7f9340455823388e1e2b943d2ca1cb
Tituspdf <titus.huebner@gmail.com>
2022-01-04 20:46:46 +0100

    created transform properties and constructor

--- Engine/WorldSpace/Transform.cs
+++ Engine/WorldSpace/Transform.cs
@@ -1,7 +1,16 @@
-﻿namespace MonoGameJam4.Engine.WorldSpace
+﻿using Microsoft.Xna.Framework;
+
+namespace MonoGameJam4.Engine.WorldSpace
 {
     public class Transform
     {
-        
+        public Vector2 Position { get; set; }
+        public Vector2 Scale { get; set; }
+
+        public Transform(Vector2 position, Vector2 scale)
+        {
+            Position = position;
+            Scale = scale;
+        }
     }
 }

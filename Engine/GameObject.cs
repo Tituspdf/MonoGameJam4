d5696779a920caaa55e0e258ce94fd768c32c2c7
Tituspdf <titus.huebner@gmail.com>
2022-01-04 20:47:44 +0100

    added transform to game object

--- Engine/GameObject.cs
+++ Engine/GameObject.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.Xna.Framework;
+using MonoGameJam4.Engine.WorldSpace;
 using IUpdateable = MonoGameJam4.Engine.Interfaces.IUpdateable;
 
 namespace MonoGameJam4.Engine
 {
     public class GameObject : IUpdateable
     {
+        protected Transform Transform;
+
+        public GameObject(Transform transform)
+        {
+            Transform = transform;
+        }
+
         public virtual void Update(GameTime gameTime)
         {
             

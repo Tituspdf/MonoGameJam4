86fdaa14cc15661828dfa324fbefe860fc1467a4
Tituspdf <titus.huebner@gmail.com>
2022-01-04 20:54:40 +0100

    created IPositionable.cs

---
+++ Engine/Interfaces/IPositionable.cs
@@ -0,0 +1,9 @@
+﻿using MonoGameJam4.Engine.WorldSpace;
+
+namespace MonoGameJam4.Engine.Interfaces
+{
+    public interface IPositionable
+    {
+        public Transform Transform { get; set; }
+    }
+}

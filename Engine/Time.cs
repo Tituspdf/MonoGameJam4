dc7dc0d4d1aeaef22177390a2ccf08fa510abcd4
Tituspdf <titus.huebner@gmail.com>
2022-01-04 20:35:26 +0100

    created time class

---
+++ Engine/Time.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MonoGameJam4.Engine
+{
+    public class Time : EngineObject
+    {
+        public static float Scale { get; set; }
+        public static float DeltaTime { get; set; }
+        public static float FixedDeltaTime { get; set; }
+
+        public Time()
+        {
+            Scale = 1;
+        }
+        
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            FixedDeltaTime = gameTime.ElapsedGameTime.Seconds;
+            DeltaTime = FixedDeltaTime * Scale;
+        }
+    }
+}

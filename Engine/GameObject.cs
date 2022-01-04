8290913c02f716fab24171d6be989c98565296c1
Tituspdf <titus.huebner@gmail.com>
2022-01-04 20:49:59 +0100

    added name to object

--- Engine/GameObject.cs
+++ Engine/GameObject.cs
@@ -7,10 +7,12 @@ namespace MonoGameJam4.Engine
     public class GameObject : IUpdateable
     {
         protected Transform Transform;
+        public string Name { get; set; }
 
-        public GameObject(Transform transform)
+        public GameObject(Transform transform, string name)
         {
             Transform = transform;
+            Name = name;
         }
 
         public virtual void Update(GameTime gameTime)

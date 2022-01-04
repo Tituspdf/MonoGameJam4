4d7645350f16f86d70892b7b3f7779b31c143d42
Tituspdf <titus.huebner@gmail.com>
2022-01-04 20:56:13 +0100

    made camera a game object

--- Engine/Rendering/Camera.cs
+++ Engine/Rendering/Camera.cs
@@ -1,7 +1,12 @@
-﻿namespace MonoGameJam4.Engine.Rendering
+﻿using MonoGameJam4.Engine.WorldSpace;
+
+namespace MonoGameJam4.Engine.Rendering
 {
-    public class Camera
+    public class Camera : GameObject
     {
-        
+        public Camera(Transform transform, string name) : base(transform, name)
+        {
+            
+        }
     }
 }

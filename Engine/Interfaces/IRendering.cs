b2a4267f41ea8d530883e6c3e4df0b7b6b1e452b
Tituspdf <titus.huebner@gmail.com>
2022-01-05 17:23:18 +0100

    created rendering interface

---
+++ Engine/Interfaces/IRendering.cs
@@ -0,0 +1,9 @@
+﻿using MonoGameJam4.Engine.Rendering;
+
+namespace MonoGameJam4.Engine.Interfaces
+{
+    public interface IRendering
+    {
+        public Renderer Renderer { get; set; }
+    }
+}

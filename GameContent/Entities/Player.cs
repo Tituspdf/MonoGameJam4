e7529bcf763ca274e755ea6ed77088fb26de4274
Tituspdf <titus.huebner@gmail.com>
2022-01-05 19:35:57 +0100

    added renderer to player

--- GameContent/Entities/Player.cs
+++ GameContent/Entities/Player.cs
@@ -1,13 +1,17 @@
 ﻿using MonoGameJam4.Engine;
+using MonoGameJam4.Engine.Interfaces;
+using MonoGameJam4.Engine.Rendering;
 using MonoGameJam4.Engine.WorldSpace;
 
 namespace MonoGameJam4.GameContent.Entities
 {
-    public class Player : GameObject
+    public class Player : GameObject, IRendering
     {
+        public Renderer Renderer { get; set; }
+        
         public Player(GameCenter gameCenter, Transform transform, string name) : base(gameCenter, transform, name)
         {
-            
+            Renderer = new Renderer(this, "Player");
         }
     }
 }

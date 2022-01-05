221ca77cd63362625a50a264c5af4c00a581ef51
Tituspdf <titus.huebner@gmail.com>
2022-01-05 17:00:23 +0100

    removed unecessary using

--- Engine/Input/InputManagement.cs
+++ Engine/Input/InputManagement.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
-using IUpdateable = MonoGameJam4.Engine.Interfaces.IUpdateable;
 
 namespace MonoGameJam4.Engine.Input
 {

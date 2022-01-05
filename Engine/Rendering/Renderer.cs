3b5c4abc7c92c3c69eb8e97be5fde89f61b5e780
Tituspdf <titus.huebner@gmail.com>
2022-01-05 06:57:07 +0100

    added necessary variables to renderer class

--- Engine/Rendering/Renderer.cs
+++ Engine/Rendering/Renderer.cs
@@ -1,7 +1,19 @@
-﻿namespace MonoGameJam4.Engine.Rendering
+﻿using Microsoft.Xna.Framework.Graphics;
+using MonoGameJam4.Engine.WorldSpace;
+
+namespace MonoGameJam4.Engine.Rendering
 {
     public class Renderer
     {
-        
+        private SpriteBatch _spriteBatch;
+        private Texture2D _texture;
+        private Transform _transform;
+
+        public Renderer(SpriteBatch spriteBatch, Texture2D texture, Transform transform)
+        {
+            _spriteBatch = spriteBatch;
+            _texture = texture;
+            _transform = transform;
+        }
     }
 }

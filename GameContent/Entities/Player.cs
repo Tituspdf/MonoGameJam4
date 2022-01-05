4a18cd01528e00466516de154ec4cb646b9114ed
Tituspdf <titus.huebner@gmail.com>
2022-01-05 17:20:56 +0100

    created player claas

---
+++ GameContent/Entities/Player.cs
@@ -0,0 +1,13 @@
+﻿using MonoGameJam4.Engine;
+using MonoGameJam4.Engine.WorldSpace;
+
+namespace MonoGameJam4.GameContent.Entities
+{
+    public class Player : GameObject
+    {
+        public Player(GameCenter gameCenter, Transform transform, string name) : base(gameCenter, transform, name)
+        {
+            
+        }
+    }
+}

3fd156fb9354daf990887ae886864fe2a2768d70
Tituspdf <titus.huebner@gmail.com>
2022-01-06 10:54:53 +0100

    added basic player movement

--- GameContent/Entities/Player.cs
+++ GameContent/Entities/Player.cs
@@ -1,4 +1,6 @@
-﻿using MonoGameJam4.Engine;
+﻿using Microsoft.Xna.Framework;
+using MonoGameJam4.Engine;
+using MonoGameJam4.Engine.Input;
 using MonoGameJam4.Engine.Interfaces;
 using MonoGameJam4.Engine.Rendering;
 using MonoGameJam4.Engine.WorldSpace;
@@ -8,10 +10,19 @@ namespace MonoGameJam4.GameContent.Entities
     public class Player : GameObject, IRendering
     {
         public Renderer Renderer { get; set; }
+        private InputManagement _input;
         
         public Player(GameCenter gameCenter, Transform transform, string name) : base(gameCenter, transform, name)
         {
             Renderer = new Renderer(this, "Player");
+            _input = gameCenter.InputManagement;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            Vector2 movement = _input.Movement;
+            Transform.Position += movement * Time.DeltaTime * 2;
         }
     }
 }

eac2cb91ddf8f67b4caf3d30f4c3d27d5372a75b
Tituspdf <titus.huebner@gmail.com>
2022-01-06 10:26:49 +0100

    added screen size property to window

--- Engine/Rendering/Window.cs
+++ Engine/Rendering/Window.cs
@@ -5,10 +5,13 @@ namespace MonoGameJam4.Engine.Rendering
     public class Window : EngineObject
     {
         private GraphicsDeviceManager _graphics;
-        
+
+        public Vector2 ScreenSize { get; private set; }
+
         public Window(GraphicsDeviceManager graphics)
         {
             _graphics = graphics;
+            ScreenSize = new Vector2(graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight);
         }
     }
 }

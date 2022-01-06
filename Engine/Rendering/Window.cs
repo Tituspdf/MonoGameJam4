a941ccb58f42a3d3c05057d7e84532cd90f87eca
Tituspdf <titus.huebner@gmail.com>
2022-01-06 10:15:47 +0100

    added graphics device manager to window class

--- Engine/Rendering/Window.cs
+++ Engine/Rendering/Window.cs
@@ -1,7 +1,14 @@
-﻿namespace MonoGameJam4.Engine.Rendering
+﻿using Microsoft.Xna.Framework;
+
+namespace MonoGameJam4.Engine.Rendering
 {
     public class Window : EngineObject
     {
+        private GraphicsDeviceManager _graphics;
         
+        public Window(GraphicsDeviceManager graphics)
+        {
+            _graphics = graphics;
+        }
     }
 }

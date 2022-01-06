5ca8bb6348b44caad43d777466ae49f897434d5e
Tituspdf <titus.huebner@gmail.com>
2022-01-06 10:43:32 +0100

    made the whole game smaller

--- Engine/Rendering/Camera.cs
+++ Engine/Rendering/Camera.cs
@@ -5,7 +5,7 @@ namespace MonoGameJam4.Engine.Rendering
     public class Camera : GameObject
     {
         public float Zoom = 1;
-        public int PixelsPerUnit = 100;
+        public int PixelsPerUnit = 50;
         
         public Camera(GameCenter gameCenter, Transform transform, string name) : base(gameCenter, transform, name)
         {

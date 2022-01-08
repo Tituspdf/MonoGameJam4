23ab6a608fd4f533f2567e2e6a76cc90e7c52337
Tituspdf <titus.huebner@gmail.com>
2022-01-08 23:14:33 +0100

    added actor methods

--- Engine/Entities/Actor.cs
+++ Engine/Entities/Actor.cs
@@ -1,7 +1,32 @@
-﻿namespace MonoGameJam4.Engine.Entities
+﻿using System;
+using MonoGameJam4.Engine.Enums;
+using MonoGameJam4.Engine.WorldSpace;
+
+namespace MonoGameJam4.Engine.Entities
 {
-    public class Actor
+    public class Actor : GameObject
     {
+        public bool Colliding;
         
+        public Actor(GameCenter gameCenter, Transform transform, string name, bool colliding) : base(gameCenter, transform, name)
+        {
+            Colliding = colliding;
+        }
+
+        private void MoveX(float amount, Action onCollision)
+        {
+            if (amount == 0) return;
+
+            foreach (var o in GameCenter.GetColliders(EntityType.Actor))
+            {
+                Actor actor = (Actor) o;
+                
+            }
+        }
+
+        private void MoveY(float amount, Action onCollision)
+        {
+            
+        }
     }
 }

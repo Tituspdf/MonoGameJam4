6ae67188efd7f95ef5edea7ea5378dc0fccf6db9
Tituspdf <titus.huebner@gmail.com>
2022-01-09 19:49:57 +0100

    fixed self collision problem

--- Engine/Entities/Actor.cs
+++ Engine/Entities/Actor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
+using MonoGameJam4.Engine.Debugging;
 using MonoGameJam4.Engine.Enums;
 using MonoGameJam4.Engine.WorldSpace;
 
@@ -8,8 +9,9 @@ namespace MonoGameJam4.Engine.Entities
     public class Actor : CollidingObject
     {
         public bool Colliding;
-        
-        public Actor(GameCenter gameCenter, Transform transform, string name, bool colliding) : base(gameCenter, transform, name)
+
+        public Actor(GameCenter gameCenter, Transform transform, string name, bool colliding) : base(gameCenter,
+            transform, name)
         {
             Colliding = colliding;
         }
@@ -19,10 +21,11 @@ namespace MonoGameJam4.Engine.Entities
             if (amount == 0) return;
 
             Vector2 move = new Vector2(amount, 0);
-            
+
             foreach (GameObject o in GameCenter.GetColliders(EntityType.Actor))
             {
                 Actor actor = (Actor) o;
+                if (actor == this) continue;
                 if (!CheckCollision(Transform.Moved(move), actor.Transform)) continue;
                 onCollision?.Invoke(actor);
                 return;
@@ -36,10 +39,11 @@ namespace MonoGameJam4.Engine.Entities
             if (amount == 0) return;
 
             Vector2 move = new Vector2(0, amount);
-            
+
             foreach (GameObject o in GameCenter.GetColliders(EntityType.Actor))
             {
                 Actor actor = (Actor) o;
+                if (actor == this) continue;
                 if (!CheckCollision(Transform.Moved(move), actor.Transform)) continue;
                 onCollision?.Invoke(actor);
                 return;

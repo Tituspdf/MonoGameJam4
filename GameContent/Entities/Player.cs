372ab119fbca0267b61be52bb0c43f71f5cb0b2d
Tituspdf <titus.huebner@gmail.com>
2022-01-12 16:53:09 +0100

    removed debugging the name on collision

--- GameContent/Entities/Player.cs
+++ GameContent/Entities/Player.cs
@@ -27,8 +27,8 @@ namespace MonoGameJam4.GameContent.Entities
             base.Update(gameTime);
             Vector2 move = _input.Movement * Time.DeltaTime * 3.5f;
             
-            MoveX(move.X, delegate(CollidingObject o) { Debug.Log(o.Name); });
-            MoveY(move.Y, delegate(CollidingObject o) { Debug.Log(o.Name); });
+            MoveX(move.X, delegate { });
+            MoveY(move.Y, delegate { });
 
             Vector2 lookDir = GameCenter.Camera.ScreenToWorldPosition() - Transform.Position;
             float angle = (float) Math.Atan2(-lookDir.Y, lookDir.X) + MathHelper.ToRadians(90); 

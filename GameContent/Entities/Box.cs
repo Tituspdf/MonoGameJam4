695643339e02b478c04a851ff8867345ec6e7c04
Tituspdf <titus.huebner@gmail.com>
2022-01-12 16:49:10 +0100

    created box class

---
+++ GameContent/Entities/Box.cs
@@ -0,0 +1,16 @@
+﻿using MonoGameJam4.Engine.Entities;
+using MonoGameJam4.Engine.Rendering;
+using MonoGameJam4.Engine.WorldSpace;
+
+namespace MonoGameJam4.GameContent.Entities
+{
+    public class Box : Actor
+    {
+        private Renderer _renderer;
+        
+        public Box(GameCenter gameCenter, Transform transform, string name, bool colliding, string textureName) : base(gameCenter, transform, name, colliding)
+        {
+            _renderer = new Renderer(this, textureName);
+        }
+    }
+}

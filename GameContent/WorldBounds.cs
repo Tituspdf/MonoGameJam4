3c2e261e71f1a2b18c93e5b17e138b174857ec22
Tituspdf <titus.huebner@gmail.com>
2022-01-12 21:18:59 +0100

    created border for the world

--- GameContent/WorldBounds.cs
+++ GameContent/WorldBounds.cs
@@ -1,5 +1,8 @@
-﻿using MonoGameJam4.Engine.Entities;
+﻿using Microsoft.Xna.Framework;
+using MonoGameJam4.Engine.Entities;
+using MonoGameJam4.Engine.Rendering;
 using MonoGameJam4.Engine.WorldSpace;
+using MonoGameJam4.GameContent.Entities;
 
 namespace MonoGameJam4.GameContent
 {
@@ -7,7 +10,29 @@ namespace MonoGameJam4.GameContent
     {
         public WorldBounds(GameCenter gameCenter, Transform transform, string name) : base(gameCenter, transform, name)
         {
+            const float thickness = 0.5f;
+
+            Camera camera = gameCenter.Camera;
+            Vector2 screenSize = gameCenter.GameWindow.ScreenSize;
+            // left border
+            GameCenter.GameObjects.Add(new Box(gameCenter,
+                new Transform(camera.ScreenToWorldPosition(new Vector2(0, screenSize.Y / 2)),
+                    new Vector2(thickness, camera.ScreenToWorldDistance(screenSize.Y)), 0), "BorderL", true, "Square"));
             
+            // right border
+            GameCenter.GameObjects.Add(new Box(gameCenter,
+                new Transform(camera.ScreenToWorldPosition(new Vector2(screenSize.X, screenSize.Y / 2)),
+                    new Vector2(thickness, camera.ScreenToWorldDistance(screenSize.Y)), 0), "BorderR", true, "Square"));
+            
+            // top border
+            GameCenter.GameObjects.Add(new Box(gameCenter,
+                new Transform(camera.ScreenToWorldPosition(new Vector2(screenSize.X / 2, 0)),
+                    new Vector2(camera.ScreenToWorldDistance(screenSize.X), thickness), 0), "BorderTop", true, "Square"));
+            
+            // bottom border
+            GameCenter.GameObjects.Add(new Box(gameCenter,
+                new Transform(camera.ScreenToWorldPosition(new Vector2(screenSize.X / 2, screenSize.Y)),
+                    new Vector2(camera.ScreenToWorldDistance(screenSize.X), thickness), 0), "BorderTop", true, "Square"));
         }
     }
 }

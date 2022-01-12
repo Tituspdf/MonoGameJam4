a4f59491f876091575b7b39e4ceb8223f12ecbb1
Tituspdf <titus.huebner@gmail.com>
2022-01-12 21:03:46 +0100

    created world bounds object

---
+++ GameContent/WorldBounds.cs
@@ -0,0 +1,13 @@
+﻿using MonoGameJam4.Engine.Entities;
+using MonoGameJam4.Engine.WorldSpace;
+
+namespace MonoGameJam4.GameContent
+{
+    public class WorldBounds : GameObject
+    {
+        public WorldBounds(GameCenter gameCenter, Transform transform, string name) : base(gameCenter, transform, name)
+        {
+            
+        }
+    }
+}

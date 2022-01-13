1a7b35110769f508b7a2b85f9507781a1a35f69d
Tituspdf <titus.huebner@gmail.com>
2022-01-13 17:01:22 +0100

    created enemy class

---
+++ GameContent/Entities/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using MonoGameJam4.Engine.Entities;
+using MonoGameJam4.Engine.WorldSpace;
+
+namespace MonoGameJam4.GameContent.Entities
+{
+    public class Enemy : Actor
+    {
+        public Enemy(GameCenter gameCenter, Transform transform, string name, bool colliding) : base(gameCenter, transform, name, colliding)
+        {
+        }
+    }
+}

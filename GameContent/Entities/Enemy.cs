a30824606abfeaf66fe2cafbd83dc92ec4539412
Tituspdf <titus.huebner@gmail.com>
2022-01-13 17:09:58 +0100

    get the player object

--- GameContent/Entities/Enemy.cs
+++ GameContent/Entities/Enemy.cs
@@ -1,12 +1,19 @@
-﻿using MonoGameJam4.Engine.Entities;
+﻿using System.Linq;
+using MonoGameJam4.Engine.Entities;
 using MonoGameJam4.Engine.WorldSpace;
 
 namespace MonoGameJam4.GameContent.Entities
 {
     public class Enemy : Actor
     {
+        private GameObject _player;
         public Enemy(GameCenter gameCenter, Transform transform, string name, bool colliding) : base(gameCenter, transform, name, colliding)
         {
+            foreach (GameObject obj in gameCenter.GameObjects.Where(obj => obj.Name == "Player"))
+            {
+                _player = obj;
+                break;
+            }
         }
     }
 }

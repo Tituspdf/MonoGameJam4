54f455fcd4cc5687420e885206255c5898892e1e
Tituspdf <titus.huebner@gmail.com>
2022-01-13 17:17:02 +0100

    created movement for bullet

--- GameContent/Entities/Bullet.cs
+++ GameContent/Entities/Bullet.cs
@@ -1,12 +1,29 @@
-﻿using MonoGameJam4.Engine.Entities;
+﻿using Microsoft.Xna.Framework;
+using MonoGameJam4.Engine;
+using MonoGameJam4.Engine.Entities;
 using MonoGameJam4.Engine.WorldSpace;
 
 namespace MonoGameJam4.GameContent.Entities
 {
     public class Bullet : Actor
     {
-        public Bullet(GameCenter gameCenter, Transform transform, string name, bool colliding) : base(gameCenter, transform, name, colliding)
+        private Vector2 _velocity;
+        public Bullet(GameCenter gameCenter, Transform transform, string name, bool colliding, Vector2 velocity) : base(gameCenter, transform, name, colliding)
         {
+            _velocity = velocity;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            Vector2 movement = _velocity * Time.DeltaTime;
+            MoveX(movement.X, OnCollision);
+            MoveY(movement.Y, OnCollision);
+        }
+
+        private void OnCollision(CollidingObject obj)
+        {
+            
         }
     }
 }

9121770d7326723ba1d2d71087b4f89eab51381a
Tituspdf <titus.huebner@gmail.com>
2022-01-13 17:14:41 +0100

    created bullet class

---
+++ GameContent/Entities/Bullet.cs
@@ -0,0 +1,12 @@
+﻿using MonoGameJam4.Engine.Entities;
+using MonoGameJam4.Engine.WorldSpace;
+
+namespace MonoGameJam4.GameContent.Entities
+{
+    public class Bullet : Actor
+    {
+        public Bullet(GameCenter gameCenter, Transform transform, string name, bool colliding) : base(gameCenter, transform, name, colliding)
+        {
+        }
+    }
+}

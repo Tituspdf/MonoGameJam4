543ad9432f1a8755598530a55bdfa7bdadf8ef09
Tituspdf <titus.huebner@gmail.com>
2022-01-14 15:02:07 +0100

    added enabled property

--- Engine/Component.cs
+++ Engine/Component.cs
@@ -5,10 +5,12 @@ namespace MonoGameJam4.Engine
     public class Component
     {
         protected GameObject GameObject;
+        public bool Enabled;
 
-        protected Component(GameObject gameObject)
+        protected Component(GameObject gameObject, bool enabled = true)
         {
             GameObject = gameObject;
+            Enabled = enabled;
         }
 
         public virtual void Deconstruct()

555bf5fc98a2992c7bb6f785d112109727490483
Tituspdf <titus.huebner@gmail.com>
2022-01-15 19:05:53 +0100

    created random class and random float method

---
+++ Engine/Mathematics/Random.cs
@@ -0,0 +1,28 @@
+﻿using MonoGameJam4.Engine.Debugging;
+using SystemRandom = System.Random;
+
+namespace MonoGameJam4.Engine.Mathematics
+{
+    public class Random
+    {
+        /// <summary>
+        /// function to generate a random float
+        /// </summary>
+        /// <param name="min">minimal value</param>
+        /// <param name="max">maximal value</param>
+        /// <returns>a random value</returns>
+        public static float RandomFloat(float min, float max)
+        {
+            if (min >= max)
+            {
+                Debug.LogError($"The minimal value ({min}) was bigger than the maximal value {max}");
+                return 0;
+            }
+
+            float range = max - min;
+            SystemRandom rand = new SystemRandom();
+            float select = (float) rand.NextDouble();
+            return min + range * select;
+        }
+    }
+}

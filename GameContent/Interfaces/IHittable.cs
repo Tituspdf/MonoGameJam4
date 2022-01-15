dfdc8cd7b249e9905de0bd3fb44fac9c62069778
Tituspdf <titus.huebner@gmail.com>
2022-01-15 21:26:05 +0100

    added hittable interfce

---
+++ GameContent/Interfaces/IHittable.cs
@@ -0,0 +1,10 @@
+﻿namespace MonoGameJam4.GameContent.Interfaces
+{
+    /// <summary>
+    /// Interface that allows objects to be hit by an bullet
+    /// </summary>
+    public interface IHittable
+    {
+        public void Hit();
+    }
+}

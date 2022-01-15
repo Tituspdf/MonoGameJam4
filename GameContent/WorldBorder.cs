2ed344ab5b229f6803845f6ae1e39e643e342173
Tituspdf <titus.huebner@gmail.com>
2022-01-15 20:51:27 +0100

    use a variable name as naming for the objects

--- GameContent/WorldBorder.cs
+++ GameContent/WorldBorder.cs
@@ -11,28 +11,28 @@ namespace MonoGameJam4.GameContent
         public WorldBorder(GameCenter gameCenter, Transform transform, string name) : base(gameCenter, transform, name)
         {
             const float thickness = 0.5f;
-
+            const string objectName = "Wall";
             Camera camera = gameCenter.Camera;
             Vector2 screenSize = gameCenter.GameWindow.ScreenSize;
             // left border
             GameCenter.GameObjects.Add(new Box(gameCenter,
                 new Transform(camera.ScreenToWorldPosition(new Vector2(0, screenSize.Y / 2)),
-                    new Vector2(thickness, camera.ScreenToWorldDistance(screenSize.Y)), 0), "BorderL", true, "Square"));
+                    new Vector2(thickness, camera.ScreenToWorldDistance(screenSize.Y)), 0), objectName, true, "Square"));
             
             // right border
             GameCenter.GameObjects.Add(new Box(gameCenter,
                 new Transform(camera.ScreenToWorldPosition(new Vector2(screenSize.X, screenSize.Y / 2)),
-                    new Vector2(thickness, camera.ScreenToWorldDistance(screenSize.Y)), 0), "BorderR", true, "Square"));
+                    new Vector2(thickness, camera.ScreenToWorldDistance(screenSize.Y)), 0), objectName, true, "Square"));
             
             // top border
             GameCenter.GameObjects.Add(new Box(gameCenter,
                 new Transform(camera.ScreenToWorldPosition(new Vector2(screenSize.X / 2, 0)),
-                    new Vector2(camera.ScreenToWorldDistance(screenSize.X), thickness), 0), "BorderTop", true, "Square"));
+                    new Vector2(camera.ScreenToWorldDistance(screenSize.X), thickness), 0), objectName, true, "Square"));
             
             // bottom border
             GameCenter.GameObjects.Add(new Box(gameCenter,
                 new Transform(camera.ScreenToWorldPosition(new Vector2(screenSize.X / 2, screenSize.Y)),
-                    new Vector2(camera.ScreenToWorldDistance(screenSize.X), thickness), 0), "BorderTop", true, "Square"));
+                    new Vector2(camera.ScreenToWorldDistance(screenSize.X), thickness), 0), objectName, true, "Square"));
         }
     }
 }

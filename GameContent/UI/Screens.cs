fe5fe4342b29c95fc7c4bbb6f560b680fff12a7d
Tituspdf <titus.huebner@gmail.com>
2022-01-16 20:54:03 +0100

    created screens class

---
+++ GameContent/UI/Screens.cs
@@ -0,0 +1,12 @@
+﻿using MonoGameJam4.Engine.Entities;
+using MonoGameJam4.Engine.WorldSpace;
+
+namespace MonoGameJam4.GameContent.UI
+{
+    public class Screens : GameObject
+    {
+        public Screens(GameCenter gameCenter, Transform transform, string name) : base(gameCenter, transform, name)
+        {
+        }
+    }
+}

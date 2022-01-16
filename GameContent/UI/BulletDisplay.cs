50f1f340a1c0165d5488f6309e90dbca2d65de85
Tituspdf <titus.huebner@gmail.com>
2022-01-16 14:08:16 +0100

    added render call

--- GameContent/UI/BulletDisplay.cs
+++ GameContent/UI/BulletDisplay.cs
@@ -1,16 +1,24 @@
-﻿using MonoGameJam4.Engine.Entities;
+﻿using Microsoft.Xna.Framework.Graphics;
+using MonoGameJam4.Engine.Entities;
+using MonoGameJam4.Engine.Interfaces;
+using MonoGameJam4.Engine.Rendering;
 using MonoGameJam4.Engine.WorldSpace;
 using MonoGameJam4.GameContent.Entities;
 
 namespace MonoGameJam4.GameContent.UI
 {
-    public class BulletDisplay : GameObject
+    public class BulletDisplay : GameObject, IRenderCall
     {
         private readonly Player _player;
         
         public BulletDisplay(GameCenter gameCenter, Transform transform, string name, Player player) : base(gameCenter, transform, name)
         {
             _player = player;
+        }
+
+        public void Render(SpriteBatch spriteBatch, Camera camera, Window gameWindow)
+        {
+            
         }
     }
 }

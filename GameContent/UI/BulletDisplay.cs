f51a11af2e42b5a0a58e38b18e548951dafde48b
Tituspdf <titus.huebner@gmail.com>
2022-01-16 14:18:43 +0100

    added textures

--- GameContent/UI/BulletDisplay.cs
+++ GameContent/UI/BulletDisplay.cs
@@ -10,10 +10,16 @@ namespace MonoGameJam4.GameContent.UI
     public class BulletDisplay : GameObject, IRenderCall
     {
         private readonly Player _player;
+        private readonly Texture2D _empty;
+        private readonly Texture2D _full;
+        private readonly Texture2D _icon;
         
         public BulletDisplay(GameCenter gameCenter, Transform transform, string name, Player player) : base(gameCenter, transform, name)
         {
             _player = player;
+            _full = gameCenter.ContentLoader.Textures["Point"];
+            _empty = gameCenter.ContentLoader.Textures["EmptyPoint"];
+            _icon = gameCenter.ContentLoader.Textures["Square"];
         }
 
         public void Render(SpriteBatch spriteBatch, Camera camera, Window gameWindow)

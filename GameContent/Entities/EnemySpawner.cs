a7ac05581f31908ce969521f436706c590a31fe8
Tituspdf <titus.huebner@gmail.com>
2022-01-16 16:34:52 +0100

    created EnemySpawner

---
+++ GameContent/Entities/EnemySpawner.cs
@@ -0,0 +1,12 @@
+﻿using MonoGameJam4.Engine.Entities;
+using MonoGameJam4.Engine.WorldSpace;
+
+namespace MonoGameJam4.GameContent.Entities
+{
+    public class EnemySpawner : GameObject
+    {
+        public EnemySpawner(GameCenter gameCenter, Transform transform, string name) : base(gameCenter, transform, name)
+        {
+        }
+    }
+}

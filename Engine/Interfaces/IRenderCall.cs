137fa94ade91996a366157bc037e71e8a3ffb15c
Tituspdf <titus.huebner@gmail.com>
2022-01-16 13:17:29 +0100

    improved summary

--- Engine/Interfaces/IRenderCall.cs
+++ Engine/Interfaces/IRenderCall.cs
@@ -4,7 +4,7 @@ using MonoGameJam4.Engine.Rendering;
 namespace MonoGameJam4.Engine.Interfaces
 {
     /// <summary>
-    /// interface that gives the derived class to be called by the <see cref="GameCenter.Draw"/> 
+    /// interface that makes the class visible for the <see cref="GameCenter.Draw"/> method from the game center 
     /// </summary>
     public interface IRenderCall
     {

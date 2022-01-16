1bb4410b79c1028f116b58126c8d7a29de3f12e9
Tituspdf <titus.huebner@gmail.com>
2022-01-16 10:59:29 +0100

    created random float class

---
+++ Engine/Mathematics/RandomFloat.cs
@@ -0,0 +1,16 @@
+﻿namespace MonoGameJam4.Engine.Mathematics
+{
+    public struct RandomFloat
+    {
+        private float _min;
+        private float _max;
+
+        public float Number => Random.RandomFloat(_min, _max);
+
+        public RandomFloat(float min, float max)
+        {
+            _min = min;
+            _max = max;
+        }
+    }
+}

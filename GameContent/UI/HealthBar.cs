5843fad6be0f7389ee8dbf569120725cf6b07919
Tituspdf <titus.huebner@gmail.com>
2022-01-16 13:20:52 +0100

    render the healthbar

--- GameContent/UI/HealthBar.cs
+++ GameContent/UI/HealthBar.cs
@@ -1,12 +1,26 @@
-﻿using MonoGameJam4.Engine.Entities;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGameJam4.Engine.Entities;
+using MonoGameJam4.Engine.Interfaces;
+using MonoGameJam4.Engine.Rendering;
 using MonoGameJam4.Engine.WorldSpace;
 
 namespace MonoGameJam4.GameContent.UI
 {
-    public class HealthBar : GameObject
+    public class HealthBar : GameObject, IRenderCall
     {
+        private Texture2D _frameTexture;
+        private Texture2D _fillingTexture;
+        
         public HealthBar(GameCenter gameCenter, Transform transform, string name) : base(gameCenter, transform, name)
         {
+            _frameTexture = gameCenter.ContentLoader.Textures["Frame"];
+            _fillingTexture = gameCenter.ContentLoader.Textures["Square"];
+        }
+
+        public void Render(SpriteBatch spriteBatch, Camera camera, Window gameWindow)
+        {
+            spriteBatch.Draw(_frameTexture, new Rectangle(new Point(50), new Point(50, 150)), Color.White);
         }
     }
 }

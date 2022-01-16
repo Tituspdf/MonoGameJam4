5e3d3166045d1ea789df0b1450df93e69827f403
Tituspdf <titus.huebner@gmail.com>
2022-01-16 21:11:57 +0100

    draw background for main menu

--- GameContent/UI/Screens.cs
+++ GameContent/UI/Screens.cs
@@ -37,7 +37,8 @@ namespace MonoGameJam4.GameContent.UI
             {
                 case GameState.MainMenu:
                 {
-                    spriteBatch.Draw(_squareTexture, new Rectangle(Point.Zero, gameWindow.ScreenSize.ToPoint()), null, Color.Black, 0, Vector2.Zero, SpriteEffects.None, 1);
+                    spriteBatch.Draw(_squareTexture, new Rectangle(Point.Zero, gameWindow.ScreenSize.ToPoint()), null, Color.Black, 0, Vector2.Zero, SpriteEffects.None, 0.9f);
+                    
                     string text = "Press 'SPACE' to play!";
                     Vector2 size = _fontNormal.MeasureString(text);
                     spriteBatch.DrawString(_fontNormal, text, new Vector2(50), Color.White, 0, Vector2.Zero,

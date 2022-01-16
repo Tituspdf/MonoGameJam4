88aa3c36601e1b8995dc462a707d42e25517b4ba
Tituspdf <titus.huebner@gmail.com>
2022-01-16 11:50:02 +0100

    created liner smoothing

---
+++ Engine/Mathematics/Smoothing.cs
@@ -0,0 +1,25 @@
+﻿using MonoGameJam4.Engine.Debugging;
+
+namespace MonoGameJam4.Engine.Mathematics
+{
+    public struct Smoothing
+    {
+        public static float Linear(float current, float destiny, float modifier)
+        {
+            if (current >= destiny)
+            {
+                return destiny;
+            }
+            if (current + modifier <= destiny)
+            {
+                return current + modifier;
+            }
+            if (current + modifier >= destiny)
+            {
+                return destiny;
+            }
+            Debug.LogError("unexpected linear smoothing behaving");
+            return current;
+        }
+    }
+}

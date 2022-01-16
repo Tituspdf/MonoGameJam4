a13b39e81acecfcbb50bd587e7ce0a83634f3daa
Tituspdf <titus.huebner@gmail.com>
2022-01-16 14:05:57 +0100

    added player

--- GameContent/UI/BulletDisplay.cs
+++ GameContent/UI/BulletDisplay.cs
@@ -1,12 +1,16 @@
 ﻿using MonoGameJam4.Engine.Entities;
 using MonoGameJam4.Engine.WorldSpace;
+using MonoGameJam4.GameContent.Entities;
 
 namespace MonoGameJam4.GameContent.UI
 {
     public class BulletDisplay : GameObject
     {
-        public BulletDisplay(GameCenter gameCenter, Transform transform, string name) : base(gameCenter, transform, name)
+        private readonly Player _player;
+        
+        public BulletDisplay(GameCenter gameCenter, Transform transform, string name, Player player) : base(gameCenter, transform, name)
         {
+            _player = player;
         }
     }
 }

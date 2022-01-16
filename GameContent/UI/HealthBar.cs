90ce726b26fd2b7302c230dae0779c3920ae60fc
Tituspdf <titus.huebner@gmail.com>
2022-01-16 13:31:22 +0100

    draw the heart next to the health bar

--- GameContent/UI/HealthBar.cs
+++ GameContent/UI/HealthBar.cs
@@ -4,23 +4,27 @@ using MonoGameJam4.Engine.Entities;
 using MonoGameJam4.Engine.Interfaces;
 using MonoGameJam4.Engine.Rendering;
 using MonoGameJam4.Engine.WorldSpace;
+using MonoGameJam4.GameContent.Entities;
 
 namespace MonoGameJam4.GameContent.UI
 {
     public class HealthBar : GameObject, IRenderCall
     {
+        private readonly Texture2D _heartTexture;
         private Texture2D _frameTexture;
         private Texture2D _fillingTexture;
-        
+
         public HealthBar(GameCenter gameCenter, Transform transform, string name) : base(gameCenter, transform, name)
         {
             _frameTexture = gameCenter.ContentLoader.Textures["Frame"];
             _fillingTexture = gameCenter.ContentLoader.Textures["Square"];
+            _heartTexture = gameCenter.ContentLoader.Textures["Heart"];
         }
 
         public void Render(SpriteBatch spriteBatch, Camera camera, Window gameWindow)
         {
-            spriteBatch.Draw(_frameTexture, new Rectangle(new Point(50), new Point(50, 150)), Color.White);
+            spriteBatch.Draw(_heartTexture, new Rectangle(new Point(50), new Point(50)), Color.White);
+            spriteBatch.Draw(_frameTexture, new Rectangle(new Point(110, 50), new Point(175, 50)), Color.White);
         }
     }
 }

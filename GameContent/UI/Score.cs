486a6aa7f3f70b638a779e54bb4b5c27deb8fac0
Tituspdf <titus.huebner@gmail.com>
2022-01-16 16:10:47 +0100

    render a hello world

--- GameContent/UI/Score.cs
+++ GameContent/UI/Score.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using MonoGameJam4.Engine.Entities;
 using MonoGameJam4.Engine.Interfaces;
 using MonoGameJam4.Engine.Rendering;
@@ -8,14 +9,20 @@ namespace MonoGameJam4.GameContent.UI
 {
     public class Score : GameObject, IRenderCall
     {
+        private readonly SpriteFont _font;
+        private readonly Vector2 _position;
+        
         public Score(GameCenter gameCenter, Transform transform, string name) : base(gameCenter, transform, name)
         {
-            
+            _font = gameCenter.ContentLoader.ScoreFont;
+            _position = new Vector2(gameCenter.GameWindow.ScreenMiddlePoint.X, 150);
         }
 
         public void Render(SpriteBatch spriteBatch, Camera camera, Window gameWindow)
         {
-            
+            string text = "Hello World";
+            Vector2 size = _font.MeasureString(text);
+            spriteBatch.DrawString(_font, text, _position - size / 2, Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
         }
     }
 }

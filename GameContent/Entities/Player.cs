ab1416b2a824d287db0049f9d90403587cb0e05f
Tituspdf <titus.huebner@gmail.com>
2022-01-16 19:48:36 +0100

    created upgrade mechanic

--- GameContent/Entities/Player.cs
+++ GameContent/Entities/Player.cs
@@ -1,17 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 using MonoGameJam4.Engine;
 using MonoGameJam4.Engine.Debugging;
 using MonoGameJam4.Engine.Entities;
 using MonoGameJam4.Engine.Input;
 using MonoGameJam4.Engine.Interfaces;
+using MonoGameJam4.Engine.Mathematics;
 using MonoGameJam4.Engine.Rendering;
+using MonoGameJam4.Engine.Rendering.ParticleEngine;
 using MonoGameJam4.Engine.WorldSpace;
 
 namespace MonoGameJam4.GameContent.Entities
 {
     public class Player : WorldObject, IRendering
     {
+        public enum PlayerState
+        {
+            /// <summary> state where the player can move freely </summary>
+            Fighting,
+            /// <summary> state where the game is waiting for the upgrade </summary>
+            Upgrading,
+            /// <summary> state where the update animation is playing </summary>
+            Waiting,
+        }
+
+        public PlayerState State;
+        private const float UpgradeTime = 2f;
+        private float _upgradeTimer;
+        
         public Renderer Renderer { get; set; }
         private InputManagement _input;
         private Vector2 _normalizedLookDirection;
@@ -21,29 +39,46 @@ namespace MonoGameJam4.GameContent.Entities
         private const float MaxHealth = 100;
         private float _currentHealth;
         private const float EnemyDamage = 15;
-        private const float HealthRegeneration = 0.01f;
+        private float HealthRegeneration = 0.01f;
+
         /// <summary> the amount of health which is left (value between 0 and 1 </summary>
         public float HealthProgress => _currentHealth / MaxHealth;
 
-        public const int MaxBullets = 5;
+        public int MaxBullets = 5;
         public int CurrentBullets;
         private float _bulletTimer;
-        private const float BulletTime = 1.5f;
+        private float BulletTime = 1.5f;
 
         public int Score { get; private set; }
+        public int CurrentLevel;
+        public int NextLevel;
 
         public Player(GameCenter gameCenter, Transform transform, string name) : base(gameCenter, transform, name, true)
         {
             Renderer = new Renderer(this, "Player");
             _input = gameCenter.InputManagement;
             _input.GetCallbackMouse(MouseElement.LeftButton).Invoked += OnMouse;
+            _input.GetCallback(Keys.Space).Invoked += OnUpgradeButton;
 
+            State = PlayerState.Fighting;
+            
             _currentHealth = MaxHealth;
 
             CurrentBullets = MaxBullets;
             _bulletTimer = BulletTime;
 
             Score = 0;
+
+            CurrentLevel = 0;
+            NextLevel = 1;
+        }
+
+        private void OnUpgradeButton()
+        {
+            if (State != PlayerState.Upgrading) return;
+            
+            State = PlayerState.Waiting;
+            _upgradeTimer = UpgradeTime;
         }
 
         private void OnMouse()
@@ -60,29 +95,56 @@ namespace MonoGameJam4.GameContent.Entities
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
-            Vector2 move = _input.Movement * Time.DeltaTime * 3.5f;
-
-            MoveX(move.X, delegate { });
-            MoveY(move.Y, delegate { });
-
-            Vector2 lookDir = GameCenter.Camera.ScreenToWorldPosition(_input.MousePosition) - Transform.Position;
-            float angle = (float) Math.Atan2(-lookDir.Y, lookDir.X) + MathHelper.ToRadians(90);
-            Transform.Rotation = angle;
-
-            _normalizedLookDirection = lookDir;
-            _normalizedLookDirection.Normalize();
-
-            ChangeHealth(HealthRegeneration);
-
-            // bullet regeneration 
-            if (MaxBullets <= CurrentBullets) return;
             
-            _bulletTimer -= Time.DeltaTime;
-
-            if (_bulletTimer < 0)
+            switch (State)
             {
-                CurrentBullets += 1;
-                _bulletTimer = BulletTime;
+                case PlayerState.Fighting:
+                {
+                    Vector2 move = _input.Movement * Time.DeltaTime * 3.5f;
+
+                    MoveX(move.X, delegate { });
+                    MoveY(move.Y, delegate { });
+
+                    Vector2 lookDir = GameCenter.Camera.ScreenToWorldPosition(_input.MousePosition) - Transform.Position;
+                    float angle = (float) Math.Atan2(-lookDir.Y, lookDir.X) + MathHelper.ToRadians(90);
+                    Transform.Rotation = angle;
+
+                    _normalizedLookDirection = lookDir;
+                    _normalizedLookDirection.Normalize();
+
+                    ChangeHealth(HealthRegeneration);
+
+                    // bullet regeneration 
+                    if (MaxBullets <= CurrentBullets) return;
+
+                    _bulletTimer -= Time.DeltaTime;
+
+                    if (_bulletTimer < 0)
+                    {
+                        CurrentBullets += 1;
+                        _bulletTimer = BulletTime;
+                    }
+                    break;
+                }
+                case PlayerState.Upgrading:
+                    break;
+                case PlayerState.Waiting:
+                {
+                    Transform.Rotation += 10f * Time.FixedDeltaTime;
+                    _upgradeTimer -= Time.FixedDeltaTime;
+                    if (_upgradeTimer <= 0)
+                    {
+                        State = PlayerState.Fighting;
+                        Time.Scale = 1;
+                        ParticleData data = new ParticleData(25, Color.White,
+                            GameCenter.ContentLoader.Textures["EmptyPoint"], new RandomFloat(1f, 1.8f), new Vector2(0.3f),
+                            new RandomFloat(1.4f, 1.9f));
+                        GameCenter.GameObjects.Add(new ParticleSystem(GameCenter, new Transform(Transform.Position, Transform.Scale, Transform.Rotation), "PlayerUpgradeParticle", data));
+                    }
+                    break;
+                }
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
         }
 
@@ -109,6 +171,22 @@ namespace MonoGameJam4.GameContent.Entities
         public void AddScore(int amount)
         {
             Score += amount;
+
+            if (Score % 5 != 0) return;
+            Time.Scale = 0;
+            State = PlayerState.Upgrading;
+
+            CurrentLevel += 1;
+                
+            HealthRegeneration *= 1.2f;
+            HealthRegeneration = MathHelper.Clamp(HealthRegeneration, 0, 0.9f);
+
+            BulletTime *= 0.8f;
+            BulletTime = MathHelper.Clamp(BulletTime,0.2f, 10);
+
+            if (Score % 10 != 0) return;
+            MaxBullets++;
+            MaxBullets = MathHelper.Clamp(MaxBullets, 5, 10);
         }
     }
 }

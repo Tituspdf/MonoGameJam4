853248af6fb40c43563954fb5b3cfa1b870d4be3
Tituspdf <titus.huebner@gmail.com>
2022-01-16 15:41:26 +0100

    created score class

---
+++ GameContent/UI/Score.cs
@@ -0,0 +1,13 @@
+﻿using MonoGameJam4.Engine.Entities;
+using MonoGameJam4.Engine.WorldSpace;
+
+namespace MonoGameJam4.GameContent.UI
+{
+    public class Score : GameObject
+    {
+        public Score(GameCenter gameCenter, Transform transform, string name) : base(gameCenter, transform, name)
+        {
+            
+        }
+    }
+}

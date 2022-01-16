dcf50174670ddd0ba97da1a97276576e492e4c05
Tituspdf <titus.huebner@gmail.com>
2022-01-16 15:41:52 +0100

    implemented render call

--- GameContent/UI/Score.cs
+++ GameContent/UI/Score.cs
@@ -1,11 +1,19 @@
-﻿using MonoGameJam4.Engine.Entities;
+﻿using Microsoft.Xna.Framework.Graphics;
+using MonoGameJam4.Engine.Entities;
+using MonoGameJam4.Engine.Interfaces;
+using MonoGameJam4.Engine.Rendering;
 using MonoGameJam4.Engine.WorldSpace;
 
 namespace MonoGameJam4.GameContent.UI
 {
-    public class Score : GameObject
+    public class Score : GameObject, IRenderCall
     {
         public Score(GameCenter gameCenter, Transform transform, string name) : base(gameCenter, transform, name)
+        {
+            
+        }
+
+        public void Render(SpriteBatch spriteBatch, Camera camera, Window gameWindow)
         {
             
         }

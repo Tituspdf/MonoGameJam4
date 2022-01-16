5b08ab20d2b3228a13b597ba00095effc9499c0f
Tituspdf <titus.huebner@gmail.com>
2022-01-16 13:14:43 +0100

    created health bar object

---
+++ GameContent/UI/HealthBar.cs
@@ -0,0 +1,12 @@
+﻿using MonoGameJam4.Engine.Entities;
+using MonoGameJam4.Engine.WorldSpace;
+
+namespace MonoGameJam4.GameContent.UI
+{
+    public class HealthBar : GameObject
+    {
+        public HealthBar(GameCenter gameCenter, Transform transform, string name) : base(gameCenter, transform, name)
+        {
+        }
+    }
+}

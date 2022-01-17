931d1df18e41508bea682190cca6d5fbc115a259
Tituspdf <titus.huebner@gmail.com>
2022-01-17 06:43:52 +0100

    added layer depth as optional parameter

--- Engine/Rendering/Renderer.cs
+++ Engine/Rendering/Renderer.cs
@@ -15,8 +15,9 @@ namespace MonoGameJam4.Engine.Rendering
         private readonly Camera _camera;
         private readonly Window _window;
         public Color Color = Color.White;
+        private readonly float _layerDepth;
         
-        public Renderer(GameObject gameObject, string textureName) : base(gameObject)
+        public Renderer(GameObject gameObject, string textureName, float layerDepth = 0) : base(gameObject)
         {
             _spriteBatch = gameObject.GameCenter.SpriteBatch;
             _textureName = textureName;
@@ -24,6 +25,7 @@ namespace MonoGameJam4.Engine.Rendering
             gameObject.GameCenter.Rendered += Render;
             _camera = gameObject.GameCenter.Camera;
             _window = gameObject.GameCenter.GameWindow;
+            _layerDepth = layerDepth;
         }
 
         private void Render()
@@ -50,7 +52,7 @@ namespace MonoGameJam4.Engine.Rendering
             
             Rectangle destinationRectangle = new Rectangle((int) (relativePosition.X), (int) (relativePosition.Y), (int) size.X, (int) size.Y);
             
-            _spriteBatch.Draw(_texture, destinationRectangle, null, Color, rotation, origin, SpriteEffects.None, 0);
+            _spriteBatch.Draw(_texture, destinationRectangle, null, Color, rotation, origin, SpriteEffects.None, _layerDepth);
             
             // https://docs.monogame.net/api/Microsoft.Xna.Framework.Graphics.SpriteBatch.html#Microsoft_Xna_Framework_Graphics_SpriteBatch_Draw_Microsoft_Xna_Framework_Graphics_Texture2D_Microsoft_Xna_Framework_Vector2_System_Nullable_Microsoft_Xna_Framework_Rectangle__Microsoft_Xna_Framework_Color_System_Single_Microsoft_Xna_Framework_Vector2_Microsoft_Xna_Framework_Vector2_Microsoft_Xna_Framework_Graphics_SpriteEffects_System_Single_
         }

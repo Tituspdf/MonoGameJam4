3aad70cbf9a5c8bb054b8b8dee402885d0726369
Tituspdf <titus.huebner@gmail.com>
2022-01-17 06:52:45 +0100

    created tutorial object

---
+++ GameContent/UI/Tutorial.cs
@@ -0,0 +1,12 @@
+﻿using MonoGameJam4.Engine.Entities;
+using MonoGameJam4.Engine.WorldSpace;
+
+namespace MonoGameJam4.GameContent.UI
+{
+    public class Tutorial : GameObject
+    {
+        public Tutorial(GameCenter gameCenter, Transform transform, string name) : base(gameCenter, transform, name)
+        {
+        }
+    }
+}
